14ed46a7b78e03b1afbe95e3c96adaee02d6b02b
heisjun <jun146517@gmail.com>
2023-08-25 15:36:56 +0900

    Feat : Add ContactPage
    컨텍츠 페이지 추가

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,17 +1,62 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import Header from "components/Layout/Header/Header";
+import { RxGithubLogo, RxNotionLogo } from "react-icons/rx";
 
 const Contact = () => {
+  const handleOpenNewTab = (url: string) => {
+    window.open(url, "_blank", "noopener, noreferrer");
+  };
   return (
     <div css={containerStyle}>
-      <h1>"It's MY Contact"</h1>
+      <Header />
+      <div css={textContainerStyle}>
+        <div css={textStyle}>Contact</div>
+        <div css={textStyle}>+82 010 2303 9598</div>
+        <div css={textStyle}>jun146517@gmail.com</div>
+
+        <div>
+          <RxNotionLogo
+            css={iconStyle}
+            onClick={() =>
+              handleOpenNewTab(
+                "https://placid-dragonfly-b6b.notion.site/265256b1636d4ab69b252f273fdba52a?pvs=4"
+              )
+            }
+          />
+          <RxGithubLogo
+            css={iconStyle}
+            onClick={() => handleOpenNewTab("https://github.com/heisjun")}
+          />
+        </div>
+      </div>
     </div>
   );
 };
 
 const containerStyle = css`
-  height: 900px;
-  padding-top: 80px;
-  background-color: peachpuff;
+  height: 100vh;
+  width: 100%;
+  background-color: black;
+  position: relative;
+  color: white;
+`;
+
+const textContainerStyle = css`
+  display: flex;
+  flex-direction: column;
+  padding: 200px;
+`;
+
+const textStyle = css`
+  font-weight: bold;
+  font-size: 2.5rem;
+`;
+
+const iconStyle = css`
+  width: 5rem;
+  height: 5rem;
+  cursor: pointer;
+  color: lightgray;
 `;
 export default Contact;
